606b79909dcf2956ee06ae0b01357989dfa615b0
AstrlJelly <bdlawson115@gmail.com>
2023-11-10 20:52:44 -0500

    mr spork is Real!
    mwhehehehe

--- scripts/characters/mr_spork/mr_spork.gd
+++ scripts/characters/mr_spork/mr_spork.gd
@@ -1,11 +1,11 @@
 extends "res://scripts/characters/character_base.gd"
 
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	start()
-	pass
+	pass # Replace with function body.
+
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _physics_process(delta):
-	movement(delta)
+func _process(delta):
 	pass
